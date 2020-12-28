00f57167240990ebaa033397a662ab7c8474431f
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-12-28 23:47:34 +0530

    updated

--- app/src/main/java/com/dissanayake/mobilelectureassigment_02/MainActivity.kt
+++ app/src/main/java/com/dissanayake/mobilelectureassigment_02/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.dissanayake.mobilelectureassigment_02
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val MyPREFERENCE = "My preference"
+    var sharedPreferences: SharedPreferences? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
